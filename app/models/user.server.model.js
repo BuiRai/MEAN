f41eec76297cf7e7691404d40dc4970ec5a34942
buirai <buirai_naetle@hotmail.com>
2015-11-27 22:38:04 -0600

    Nes Schema Post

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -6,14 +6,28 @@ var UserSchema = new Schema({
 	lastName: String,
 	email: {
 		type: String,
-		index: true		
+		index: true,
+		match: /.+\@.+\..+/	
 	},
 	username: {
 		type: String,
 		trim: true,
-		unique: true
+		unique: true,
+		required: true
 	},
-	password: String,
+	password:{
+		type: String,
+		validate: [
+			function(password) {
+				return password.length >= 6;
+			},
+			'Password should be longer'
+		]
+	},
+	role: {
+		type: String,
+		enum: ['Admin', 'Owner', 'User']
+	},
 	website: {
 		type: String,
 		get: function(url) {
