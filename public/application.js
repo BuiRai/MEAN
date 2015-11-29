1a7b1250cefe6e7e08ebd2c04511324dda00e172
buirai <buirai_naetle@hotmail.com>
2015-11-29 14:36:36 -0600

    Angular routing example success

--- public/application.js
+++ public/application.js
@@ -1,6 +1,17 @@
 var mainApplicationModuleName = 'mean';
 
-var mainApplicationModule = angular.module(mainApplicationModuleName, ['example']);
+/*
+* Dependencies:
+	ngRoute
+	Example [my module]
+*/
+var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngRoute', 'example']);
+
+mainApplicationModule.config(['$locationProvider',
+	function($locationProvider) {
+		$locationProvider.hashPrefix('!');
+	}
+]);
 
 angular.element(document).ready(function() {
 	angular.bootstrap(document, [mainApplicationModuleName]);
