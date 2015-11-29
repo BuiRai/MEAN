99f6d3acb1b6aa0bf2c18c06594a79e5ba9496d4
buirai <buirai_naetle@hotmail.com>
2015-11-29 14:55:28 -0600

    Authentication success with Angular

--- public/application.js
+++ public/application.js
@@ -5,13 +5,15 @@ var mainApplicationModuleName = 'mean';
 	ngRoute
 	Example [my module]
 */
-var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngRoute', 'example']);
+var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngRoute', 'users', 'example']);
 
 mainApplicationModule.config(['$locationProvider',
 	function($locationProvider) {
 		$locationProvider.hashPrefix('!');
 	}
 ]);
+
+if (window.location.hash === '#_=_') window.location.hash = '#!';
 
 angular.element(document).ready(function() {
 	angular.bootstrap(document, [mainApplicationModuleName]);
