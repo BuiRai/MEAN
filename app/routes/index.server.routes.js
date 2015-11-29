3de9832e7e931186ce55d01dea193cc4e884ed94
buirai <buirai_naetle@hotmail.com>
2015-11-28 23:04:27 -0600

    Wiring the user's routes

--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -1,4 +1,6 @@
-module.exports = function(app) {
-	var index = require('../controllers/index.server.controller');
-	app.get('/', index.render);
+exports.render = function(req, res) {
+	res.render('index', {
+		title: 'Hello World',
+		userFullName: req.user ? req.user.fullName : ''
+	});
 };
--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -1,14 +1,18 @@
-var users = require('../../app/controllers/users.server.controller');
+var users = require('../../app/controllers/users.server.controller'),
+	passport = require('passport');
 
 module.exports = function(app) {
-	app.route('/users')
-		.post(users.create)
-		.get(users.list);
-
-	app.route('/users/:userId')
-		.get(users.read)
-		.put(users.update) //Update an user
-		.delete(users.delete); //Delete an user
-
-	app.param('userId', users.userByID);
+	app.route('/signup')
+		.get(users.renderSignup)
+		.post(users.signup);
+	
+	app.route('/signin')
+		.get(users.renderSignin)
+		.post(passport.authenticate('local', {
+			successRedirect: '/',
+			failureRedirect: '/signin',
+			failureFlash: true
+		}));
+		
+	app.get('/signout', users.signout);
 };
