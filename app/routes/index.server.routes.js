5b776e11b4c2dab03376af58958403c00d79c39d
buirai <buirai_naetle@hotmail.com>
2015-11-29 10:00:54 -0600

    login ready

--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -1,6 +1,5 @@
-exports.render = function(req, res) {
-	res.render('index', {
-		title: 'Hello World',
-		userFullName: req.user ? req.user.fullName : ''
-	});
+module.exports = function (app) {
+  	var index = require('../controllers/index.server.controller');
+
+  	app.get('/', index.render);
 };
--- config/express.js
+++ config/express.js
@@ -10,20 +10,17 @@ var config = require('./config'),
 
 module.exports = function() {
 	var app = express();
-
 	if (process.env.NODE_ENV === 'development') {
 		app.use(morgan('dev'));
 	} else if (process.env.NODE_ENV === 'production') {
 		app.use(compress());
 	}
-
 	app.use(bodyParser.urlencoded({
 		extended: true
 	}));
 
 	app.use(bodyParser.json());
 	app.use(methodOverride());
-
 	app.use(session({
 		saveUninitialized: true,
 		resave: true,
@@ -39,7 +36,7 @@ module.exports = function() {
 
 	require('../app/routes/index.server.routes.js')(app);
 	require('../app/routes/users.server.routes.js')(app);
-	
+
 	app.use(express.static('./public'));
 
 	return app;
