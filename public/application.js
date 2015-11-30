8ee80ba987852f28b467fd3b332af3812a4b3fc9
buirai <buirai_naetle@hotmail.com>
2015-11-30 14:40:09 -0600

    Add lumX

--- public/application.js
+++ public/application.js
@@ -6,7 +6,7 @@ var mainApplicationModuleName = 'mean';
 	Example [my module]
 */
 var mainApplicationModule = angular.module(mainApplicationModuleName,
-	['ngResource', 'ngRoute', 'users', 'example', 'articles']);
+	['ngResource', 'ngRoute', 'users', 'example', 'articles', 'lumx']);
 
 mainApplicationModule.config(['$locationProvider',
 	function($locationProvider) {
